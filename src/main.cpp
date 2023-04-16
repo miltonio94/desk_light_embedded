3cc4fab5834b27156e41b81c498ecac8f5d4b381
Milton <milton94@gmail.com>
2023-04-16 14:30:15 +0100

    Init project with basic on off neopixels
    Starting point for desk_lamp.
    Very simple starting point with a main.cpp where neopixels are set to blue
    and we turn it on and off via a button.
    No debouncing has been implemented, this is an IOT project so the button
    is just a stand in for the network operations that will eventually take place.
    
    TODO: Next step is to replace the button interface with websocket

---
+++ src/main.cpp
@@ -0,0 +1,42 @@
+#include "Adafruit_NeoPixel.h"
+#include "Arduino.h"
+
+#define NUMBER_OF_PIXELS 10
+#define PIXEL_PIN D3
+#define  BUTTON_PIN D1
+
+Adafruit_NeoPixel pixels(NUMBER_OF_PIXELS, D3, NEO_GRB + NEO_KHZ800);
+
+uint8_t buttonLastState = HIGH;
+uint8_t buttonCurrentState = HIGH;
+bool buttonToggleState = HIGH;
+
+void setup() {
+    Serial.begin(9600);
+    pinMode(BUTTON_PIN, INPUT_PULLUP);
+    pixels.begin();
+}
+
+void loop() {
+    pixels.clear();
+
+    // Toggling button state on and off! Debounce is not implemented
+    // digitalRead reads high unless the button is pressed down
+    // so I'm iverting that here
+    if(!digitalRead(BUTTON_PIN)){
+        if(!buttonCurrentState){
+            buttonCurrentState = true;
+        } else buttonCurrentState = false;
+
+    }
+
+    if (buttonCurrentState) {
+        pixels.setBrightness(255);
+        for (int8 i = 0; i < NUMBER_OF_PIXELS; i++) {
+            pixels.setPixelColor(i, pixels.Color(0, 0, 255));
+        }
+
+    }
+
+    pixels.show();
+}
