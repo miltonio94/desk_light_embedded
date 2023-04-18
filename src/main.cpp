6b60741342d69ecfc186bc53788b595b038bc078
Milton <milton94@gmail.com>
2023-04-18 22:57:28 +0100

    Bug fix - NeoPixels now don't break when reseting

--- src/main.cpp
+++ src/main.cpp
@@ -3,40 +3,45 @@
 
 #define NUMBER_OF_PIXELS 10
 #define PIXEL_PIN D3
-#define  BUTTON_PIN D1
+#define BUTTON_PIN D1
 
-Adafruit_NeoPixel pixels(NUMBER_OF_PIXELS, D3, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixels(500, D3, NEO_GRB + NEO_KHZ800);
 
 uint8_t buttonLastState = HIGH;
 uint8_t buttonCurrentState = HIGH;
 bool buttonToggleState = HIGH;
 
 void setup() {
-    Serial.begin(9600);
-    pinMode(BUTTON_PIN, INPUT_PULLUP);
-    pixels.begin();
+  Serial.begin(9600);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  pixels.begin();
+
+  // doing this to fix a weird bug where when the microcontroller is reset
+  // it fails to drive the pixels properly until it's reset
+  pixels.clear();
+  pixels.show();
+  pixels.updateLength(NUMBER_OF_PIXELS);
 }
 
 void loop() {
-    pixels.clear();
+  pixels.clear();
 
-    // Toggling button state on and off! Debounce is not implemented
-    // digitalRead reads high unless the button is pressed down
-    // so I'm iverting that here
-    if(!digitalRead(BUTTON_PIN)){
-        if(!buttonCurrentState){
-            buttonCurrentState = true;
-        } else buttonCurrentState = false;
+  // Toggling button state on and off! Debounce is not implemented
+  // digitalRead reads high unless the button is pressed down
+  // so I'm iverting that here
+  if (!digitalRead(BUTTON_PIN)) {
+    if (!buttonCurrentState) {
+      buttonCurrentState = true;
+    } else
+      buttonCurrentState = false;
+  }
 
+  if (buttonCurrentState) {
+    pixels.setBrightness(255);
+    for (int8 i = 0; i < NUMBER_OF_PIXELS; i++) {
+      pixels.setPixelColor(i, pixels.Color(0, 0, 255));
     }
+  }
 
-    if (buttonCurrentState) {
-        pixels.setBrightness(255);
-        for (int8 i = 0; i < NUMBER_OF_PIXELS; i++) {
-            pixels.setPixelColor(i, pixels.Color(0, 0, 255));
-        }
-
-    }
-
-    pixels.show();
+  pixels.show();
 }
