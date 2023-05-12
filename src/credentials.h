2ed2ff563db2cf7ecf3cb558c8b0f00186ff7aeb
Milton De Paula <8180004+miltonio94@users.noreply.github.com>
2023-05-12 21:50:29 +0000

    Update credentials.h

--- src/credentials.h
+++ src/credentials.h
@@ -1,12 +1,7 @@
 #ifndef CREDENTIALS_H_
 #define CREDENTIALS_H_
 
-<<<<<<< Updated upstream
 #define SSID "<SSID_HERE"
 #define SSID_PASSWORD "<PASSWORD_HERE>"
-=======
-#define SSID_NAME "<SSID>"
-#define SSID_PASSWORD "<PASSWORD>"
->>>>>>> Stashed changes
 
 #endif // CREDENTIALS_H_
