aafcb16c4a04013ad469102dd4157c89edcc049a
Milton <miltonio94@gmail.com>
2023-05-23 05:40:59 +0000

    PIXIE-8 removing debug serial print for performance

--- src/pixie.cpp
+++ src/pixie.cpp
@@ -55,23 +55,18 @@ void setupWifi() {
   while (WiFi.status() != WL_CONNECTED) {
     delay(5);
   }
-  Serial.println("Conected!");
 }
 
 void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload,
                     size_t length) {
   switch (type) {
   case WStype_DISCONNECTED:
-    Serial.printf("[%u] Disconected!\n", num);
     websocketConnected = false;
     break;
 
   case WStype_CONNECTED: {
     websocketConnected = true;
     lastUpdate = millis();
-    IPAddress ip = webSocket.remoteIP(num);
-    Serial.printf("[%u] Connected from %d.%d.%d.%ds url: %s\n", num, ip[0],
-                  ip[1], ip[2], ip[3], payload);
 
     uint8_t colourVal[5];
 
@@ -89,17 +84,15 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload,
       webSocket.sendTXT(num, "STATE_OFF");
   } break;
 
-  case WStype_TEXT:
+  case WStype_TEXT: {
     lastUpdate = millis();
 
     if (payload[0] == 'O' && payload[1] == 'N') {
       buttonWebSocketState = true;
-      Serial.println("Turning on lamp");
       webSocket.sendTXT(num, "LAMP ON!");
       return;
     }
     if (payload[0] == 'O' && payload[1] == 'F' && payload[2] == 'F') {
-      Serial.println("Turning off lamp");
       webSocket.sendTXT(num, "LAMP OFF!");
       buttonWebSocketState = false;
       return;
@@ -122,32 +115,25 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload,
       rgb.b = (uint8_t)strtol((char *)payload + 2, &ptr, 10);
       return;
     }
-    break;
+  } break;
   }
+}
 
-  void setupSocket() {
-    Serial.println("WebSocket complete uri is: ");
-    Serial.print("ws://");
-    Serial.print(WiFi.localIP());
-    Serial.print(":");
-    Serial.print(WEBSOCKET_PORT);
-    Serial.println("/");
+void setupSocket() {
+  webSocket.begin();
+  webSocket.onEvent(webSocketEvent);
+}
 
-    webSocket.begin();
-    webSocket.onEvent(webSocketEvent);
+void setup_mDNS() {
+  if (MDNS.begin("pixie", WiFi.localIP()))
+    Serial.println("mDns started");
+  MDNS.addService("ws", "tcp", WEBSOCKET_PORT);
+}
+
+void socketUpdate() {
+  webSocket.loop();
+  if (lastUpdate + messageInterval < millis() && websocketConnected) {
+    webSocket.broadcastTXT("Simple broadcast client message!!");
+    lastUpdate = millis();
   }
-
-  void setup_mDNS() {
-    if (MDNS.begin("pixie", WiFi.localIP()))
-      Serial.println("mDns started");
-    MDNS.addService("ws", "tcp", WEBSOCKET_PORT);
-  }
-
-  void socketUpdate() {
-    webSocket.loop();
-    if (lastUpdate + messageInterval < millis() && websocketConnected) {
-      Serial.println("[WSc] SENT: Simple broadcast client message!!");
-      webSocket.broadcastTXT("Simple broadcast client message!!");
-      lastUpdate = millis();
-    }
-  }
+}
